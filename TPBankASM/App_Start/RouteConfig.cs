2c37be0f1aa7c1bbae086be5eeb7343ff8a36bd8
hsang28999 <sanghd00541@fpt.edu.vn>
2019-01-03 17:13:55 +0700

    update

--- TPBankASM/App_Start/RouteConfig.cs
+++ TPBankASM/App_Start/RouteConfig.cs
@@ -46,7 +46,7 @@ namespace TPBankASM
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "menu", id = UrlParameter.Optional }
             );
         }
     }
